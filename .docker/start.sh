c5d6d4deff6b590fe416a2cd6987aafd34a34b07
Celso Pires <celso.pires1999@gmail.com>
2025-04-14 23:08:05 +0000

    start.sh com parâmetros para sdkman

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo 'export SDKMAN_DIR="$HOME/.sdkman"' >> /home/dev/.bashrc
+echo '[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"' >> /home/dev/.bashrc
+
+echo 'export SDKMAN_DIR="$HOME/.sdkman"' >> /home/dev/.zshrc
+echo '[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"' >> /home/dev/.zshrc
+
 tail -f /dev/null
