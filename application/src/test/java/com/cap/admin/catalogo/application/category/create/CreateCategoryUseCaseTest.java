86641198ffdbcc21d3d32b53b8307f885ebe3ac1
Celso Pires <celso.pires1999@gmail.com>
2025-04-18 15:41:27 +0000

    mock clean up

--- application/src/test/java/com/cap/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/cap/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.when;
 import java.util.Objects;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -26,6 +27,11 @@ public class CreateCategoryUseCaseTest {
 
         @Mock
         private CategoryGateway categoryGateway;
+
+        @BeforeEach
+        void cleanUp() {
+                Mockito.reset(categoryGateway);
+        }
 
         @Test
         public void givenAValidCommand_whenCallsCreateCategory_thenShouldReturnCategoryId() {
--- application/src/test/java/com/cap/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
+++ application/src/test/java/com/cap/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -31,6 +32,11 @@ public class UpdateCategoryUseCaseTest {
 
         @Mock
         private CategoryGateway categoryGateway;
+
+        @BeforeEach
+        void cleanUp() {
+                Mockito.reset(categoryGateway);
+        }
 
         @Test
         void givenValidCommand_whenExecute_thenShouldReturnSuccess() {
