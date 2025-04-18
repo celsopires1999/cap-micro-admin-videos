6df723926a1f014b479ac754b3503a18b8c29b46
Celso Pires <celso.pires1999@gmail.com>
2025-04-18 20:16:37 +0000

    2.18-implementando-o-caso-de-uso-de-get-de-categoria-por-id

---
+++ application/src/main/java/com/cap/admin/catalogo/application/category/retrieve/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,7 @@
+package com.cap.admin.catalogo.application.category.retrieve.get;
+
+import com.cap.admin.catalogo.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase
+        extends UseCase<String, CategoryOutput> {
+}
