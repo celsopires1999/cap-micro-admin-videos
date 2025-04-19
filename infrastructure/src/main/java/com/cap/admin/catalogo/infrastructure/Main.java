104a0e23ee248948845fd296826220b3df2e7b73
Celso Pires <celso.pires1999@gmail.com>
2025-04-19 22:57:32 +0000

    4.03-criando-o-jpa-repository-da-categoria

--- infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/Main.java
@@ -1,9 +1,16 @@
 package com.cap.admin.catalogo.infrastructure;
 
+import java.util.List;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
+import com.cap.admin.catalogo.domain.category.Category;
+import com.cap.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.cap.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.cap.admin.catalogo.infrastructure.configuration.WebServerConfig;
 
 @SpringBootApplication
@@ -16,5 +23,20 @@ public class Main {
         // System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME,
         // "test");
         SpringApplication.run(WebServerConfig.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(CategoryRepository repository) {
+        return args -> {
+            List<CategoryJpaEntity> all = repository.findAll();
+
+            System.out.println("All categories: " + all);
+
+            Category filmes = Category.newCategory("Filmes", null, true);
+
+            repository.saveAndFlush(CategoryJpaEntity.from(filmes));
+
+            repository.deleteAll();
+        };
     }
 }
