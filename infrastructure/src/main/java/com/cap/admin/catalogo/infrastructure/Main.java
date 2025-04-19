33419ef4ce38bfdaf550e2e06e5cad4daf938a8b
Celso Pires <celso.pires1999@gmail.com>
2025-04-19 03:25:17 +0000

    3.03-configurando-os-profiles-do-springboot

--- infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/Main.java
@@ -2,6 +2,7 @@ package com.cap.admin.catalogo.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 import com.cap.admin.catalogo.infrastructure.configuration.WebServerConfig;
 
@@ -10,6 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
