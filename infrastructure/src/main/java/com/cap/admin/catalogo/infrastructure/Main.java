4694728d6ee86b80d791f55f8d1e83a3b7527cee
Celso Pires <celso.pires1999@gmail.com>
2025-04-19 18:42:03 +0000

    3.06-configurando-o-datasource-de-testes-da-aplicacao

--- infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/Main.java
@@ -11,7 +11,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME,
+                "development");
+        // System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME,
+        // "test");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
