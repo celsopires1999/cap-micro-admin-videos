6c1cd5f36dd642a13cc476e360fb71eb5b6d4405
Celso Pires <celso.pires1999@gmail.com>
2025-04-27 22:06:35 +0000

    6.02-criando-controller-de-categoria

---
+++ infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -0,0 +1,20 @@
+package com.cap.admin.catalogo.infrastructure.api.controllers;
+
+import com.cap.admin.catalogo.domain.pagination.Pagination;
+import com.cap.admin.catalogo.infrastructure.api.CategoryAPI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController implements CategoryAPI {
+
+    @Override
+    public ResponseEntity<?> createCategory() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> listCategories(String search, int page, int perPage, String sort, String direction) {
+        return null;
+    }
+}
