f62ff581951d608924d41d3991d7b8a888b864aa
Celso Pires <celso.pires1999@gmail.com>
2025-04-28 00:39:18 +0000

    6.06-testes-integrados-do-endpoint-de-criacao-de-categoria

--- application/src/main/java/com/cap/admin/catalogo/application/category/create/CreateCategoryOutput.java
+++ application/src/main/java/com/cap/admin/catalogo/application/category/create/CreateCategoryOutput.java
@@ -6,8 +6,11 @@ import com.cap.admin.catalogo.domain.category.CategoryID;
 public record CreateCategoryOutput(
         CategoryID id) {
 
-    public static CreateCategoryOutput from(
-            final Category aCategory) {
+    public static CreateCategoryOutput from(final CategoryID anId) {
+        return new CreateCategoryOutput(anId);
+    }
+
+    public static CreateCategoryOutput from(final Category aCategory) {
         return new CreateCategoryOutput(aCategory.getId());
     }
 }
