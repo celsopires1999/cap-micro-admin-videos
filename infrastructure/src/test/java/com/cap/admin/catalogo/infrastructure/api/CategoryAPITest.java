6f5c142c57af3d5d0d540d0eb509146fc5621d47
Celso Pires <celso.pires1999@gmail.com>
2025-04-29 00:38:01 +0000

    6.08-implementando-handler-de-erro-global

--- infrastructure/src/test/java/com/cap/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/cap/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -1,7 +1,10 @@
 package com.cap.admin.catalogo.infrastructure.api;
 
+import static io.vavr.API.Left;
 import static io.vavr.API.Right;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.times;
@@ -24,45 +27,107 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.cap.admin.catalogo.ControllerTest;
 import com.cap.admin.catalogo.application.category.create.CreateCategoryOutput;
 import com.cap.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.cap.admin.catalogo.domain.exceptions.DomainException;
+import com.cap.admin.catalogo.domain.validation.Error;
+import com.cap.admin.catalogo.domain.validation.handler.Notification;
 import com.cap.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ControllerTest(controllers = CategoryAPI.class)
 public class CategoryAPITest {
 
-    @Autowired
-    private MockMvc mvc;
+        @Autowired
+        private MockMvc mvc;
 
-    @Autowired
-    private ObjectMapper mapper;
+        @Autowired
+        private ObjectMapper mapper;
 
-    @MockitoBean
-    private CreateCategoryUseCase createCategoryUseCase;
+        @MockitoBean
+        private CreateCategoryUseCase createCategoryUseCase;
 
-    @Test
-    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
-        final var expectedName = "Movies";
-        final var expectedDescription = "The most watched movies";
-        final var expectedIsActive = true;
+        @Test
+        public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
+                final var expectedName = "Movies";
+                final var expectedDescription = "The most watched movies";
+                final var expectedIsActive = true;
 
-        final var aInput = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+                final var aInput = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
 
-        when(createCategoryUseCase.execute(any()))
-                .thenReturn(Right(CreateCategoryOutput.from("123")));
+                when(createCategoryUseCase.execute(any()))
+                                .thenReturn(Right(CreateCategoryOutput.from("123")));
 
-        final var request = post("/categories")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(this.mapper.writeValueAsString(aInput));
+                final var request = post("/categories")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(this.mapper.writeValueAsString(aInput));
 
-        this.mvc.perform(request)
-                .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(header().string("Location", "/categories/123"))
-                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(jsonPath("$.id", equalTo("123")));
+                this.mvc.perform(request)
+                                .andDo(print())
+                                .andExpect(status().isCreated())
+                                .andExpect(header().string("Location", "/categories/123"))
+                                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                                .andExpect(jsonPath("$.id", equalTo("123")));
 
-        verify(createCategoryUseCase, times(1)).execute(argThat(cmd -> Objects.equals(expectedName, cmd.name())
-                && Objects.equals(expectedDescription, cmd.description())
-                && Objects.equals(expectedIsActive, cmd.isActive())));
-    }
+                verify(createCategoryUseCase, times(1)).execute(argThat(cmd -> Objects.equals(expectedName, cmd.name())
+                                && Objects.equals(expectedDescription, cmd.description())
+                                && Objects.equals(expectedIsActive, cmd.isActive())));
+        }
+
+        @Test
+        public void givenAInvalidName_whenCallsCreateCategory_thenShouldReturnNotification() throws Exception {
+                final String expectedName = null;
+                final var expectedDescription = "The most watched category";
+                final var expectedIsActive = true;
+                final var expectedMessage = "'name' should not be null";
+
+                final var aInput = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+                when(createCategoryUseCase.execute(any()))
+                                .thenReturn(Left(Notification.create(new Error(expectedMessage))));
+
+                final var request = post("/categories")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(this.mapper.writeValueAsString(aInput));
+
+                this.mvc.perform(request)
+                                .andDo(print())
+                                .andExpect(status().isUnprocessableEntity())
+                                .andExpect(header().string("Location", nullValue()))
+                                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                                .andExpect(jsonPath("$.errors", hasSize(1)))
+                                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)));
+
+                verify(createCategoryUseCase, times(1)).execute(argThat(cmd -> Objects.equals(expectedName, cmd.name())
+                                && Objects.equals(expectedDescription, cmd.description())
+                                && Objects.equals(expectedIsActive, cmd.isActive())));
+        }
+
+        @Test
+        public void givenAInvalidCommand_whenCallsCreateCategory_thenShouldReturnDomainException() throws Exception {
+                final String expectedName = null;
+                final var expectedDescription = "The most watched category";
+                final var expectedIsActive = true;
+                final var expectedMessage = "'name' should not be null";
+
+                final var aInput = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+                when(createCategoryUseCase.execute(any()))
+                                .thenThrow(DomainException.with(new Error(expectedMessage)));
+
+                final var request = post("/categories")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(this.mapper.writeValueAsString(aInput));
+
+                this.mvc.perform(request)
+                                .andDo(print())
+                                .andExpect(status().isUnprocessableEntity())
+                                .andExpect(header().string("Location", nullValue()))
+                                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                                .andExpect(jsonPath("$.message", equalTo(expectedMessage)))
+                                .andExpect(jsonPath("$.errors", hasSize(1)))
+                                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)));
+
+                verify(createCategoryUseCase, times(1)).execute(argThat(cmd -> Objects.equals(expectedName, cmd.name())
+                                && Objects.equals(expectedDescription, cmd.description())
+                                && Objects.equals(expectedIsActive, cmd.isActive())));
+        }
 }
