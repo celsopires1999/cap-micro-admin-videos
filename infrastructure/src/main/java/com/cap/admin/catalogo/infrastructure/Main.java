3776061ddedb19cf66372e686b6764509a6f8a9c
Celso Pires <celso.pires1999@gmail.com>
2025-05-21 02:45:51 +0000

    7.08-testes-de-regressao-manual

--- infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/Main.java
@@ -10,7 +10,6 @@ import com.cap.admin.catalogo.infrastructure.configuration.WebServerConfig;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME,
                 "development");
         SpringApplication.run(WebServerConfig.class, args);
