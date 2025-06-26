e8db7e7d4512edb9671c2f4a9e3818e01823dc94
Celso Pires <celso.pires1999@gmail.com>
2025-06-26 01:03:18 +0000

    23.11-implementando-o-media-resource-gateway

--- infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/configuration/StorageConfig.java
+++ infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/configuration/StorageConfig.java
@@ -1,17 +1,25 @@
 package com.cap.admin.catalogo.infrastructure.configuration;
 
 import com.cap.admin.catalogo.infrastructure.configuration.properties.google.GoogleStorageProperties;
+import com.cap.admin.catalogo.infrastructure.configuration.properties.storage.StorageProperties;
 import com.cap.admin.catalogo.infrastructure.services.StorageService;
 import com.cap.admin.catalogo.infrastructure.services.impl.GCStorageService;
 import com.cap.admin.catalogo.infrastructure.services.local.InMemoryStorageService;
 import com.google.cloud.storage.Storage;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class StorageConfig {
+
+    @Bean
+    @ConfigurationProperties(value = "storage.catalogo-videos")
+    public StorageProperties storageProperties() {
+        return new StorageProperties();
+    }
 
     @Bean
     @Profile({ "production", "development" })
---
+++ infrastructure/src/main/java/com/cap/admin/catalogo/infrastructure/video/DefaultMediaResourceGateway.java
@@ -0,0 +1,67 @@
+package com.cap.admin.catalogo.infrastructure.video;
+
+import org.springframework.stereotype.Component;
+
+import com.cap.admin.catalogo.domain.resource.Resource;
+import com.cap.admin.catalogo.domain.video.AudioVideoMedia;
+import com.cap.admin.catalogo.domain.video.ImageMedia;
+import com.cap.admin.catalogo.domain.video.MediaResourceGateway;
+import com.cap.admin.catalogo.domain.video.VideoID;
+import com.cap.admin.catalogo.domain.video.VideoMediaType;
+import com.cap.admin.catalogo.domain.video.VideoResource;
+import com.cap.admin.catalogo.infrastructure.configuration.properties.storage.StorageProperties;
+import com.cap.admin.catalogo.infrastructure.services.StorageService;
+
+@Component
+public class DefaultMediaResourceGateway implements MediaResourceGateway {
+
+    private final String filenamePattern;
+    private final String locationPattern;
+    private final StorageService storageService;
+
+    public DefaultMediaResourceGateway(final StorageProperties props, final StorageService storageService) {
+        this.filenamePattern = props.getFilenamePattern();
+        this.locationPattern = props.getLocationPattern();
+        this.storageService = storageService;
+    }
+
+    @Override
+    public AudioVideoMedia storeAudioVideo(final VideoID anId, final VideoResource videoResource) {
+        final var filepath = filepath(anId, videoResource);
+        final var aResource = videoResource.resource();
+        store(filepath, aResource);
+        return AudioVideoMedia.with(aResource.checksum(), aResource.name(), filepath);
+    }
+
+    @Override
+    public ImageMedia storeImage(final VideoID anId, final VideoResource videoResource) {
+        final var filepath = filepath(anId, videoResource);
+        final var aResource = videoResource.resource();
+        store(filepath, aResource);
+        return ImageMedia.with(aResource.checksum(), aResource.name(), filepath);
+    }
+
+    @Override
+    public void clearResources(final VideoID anId) {
+        final var ids = this.storageService.list(folder(anId));
+        this.storageService.deleteAll(ids);
+    }
+
+    private String filename(final VideoMediaType aType) {
+        return filenamePattern.replace("{type}", aType.name());
+    }
+
+    private String folder(final VideoID anId) {
+        return locationPattern.replace("{videoId}", anId.getValue());
+    }
+
+    private String filepath(final VideoID anId, final VideoResource aResource) {
+        return folder(anId)
+                .concat("/")
+                .concat(filename(aResource.type()));
+    }
+
+    private void store(final String filepath, final Resource aResource) {
+        this.storageService.store(filepath, aResource);
+    }
+}
