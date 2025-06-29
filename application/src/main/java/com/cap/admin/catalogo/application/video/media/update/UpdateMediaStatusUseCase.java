5184deef7310024f8e8bee3365b5092d8608be1a
Celso Pires <celso.pires1999@gmail.com>
2025-06-29 11:58:13 -0300

    24.07-implementando-o-caso-de-uso-de-update-media-status

---
+++ application/src/main/java/com/cap/admin/catalogo/application/video/media/update/UpdateMediaStatusUseCase.java
@@ -0,0 +1,6 @@
+package com.cap.admin.catalogo.application.video.media.update;
+
+import com.cap.admin.catalogo.application.UnitUseCase;
+
+public abstract class UpdateMediaStatusUseCase extends UnitUseCase<UpdateMediaStatusCommand> {
+}
