33adf59f55f25269b83987aa10c417584529229e
Celso Pires <celso.pires1999@gmail.com>
2025-06-29 15:26:46 +0000

    24.08-limpando-storage-no-caso-de-uso-delete-video

--- application/src/main/java/com/cap/admin/catalogo/application/video/delete/DefaultDeleteVideoUseCase.java
+++ application/src/main/java/com/cap/admin/catalogo/application/video/delete/DefaultDeleteVideoUseCase.java
@@ -1,20 +1,27 @@
 package com.cap.admin.catalogo.application.video.delete;
 
+import java.util.Objects;
+
+import com.cap.admin.catalogo.domain.video.MediaResourceGateway;
 import com.cap.admin.catalogo.domain.video.VideoGateway;
 import com.cap.admin.catalogo.domain.video.VideoID;
-
-import java.util.Objects;
 
 public class DefaultDeleteVideoUseCase extends DeleteVideoUseCase {
 
     private final VideoGateway videoGateway;
+    private final MediaResourceGateway mediaResourceGateway;
 
-    public DefaultDeleteVideoUseCase(final VideoGateway videoGateway) {
+    public DefaultDeleteVideoUseCase(
+            final VideoGateway videoGateway,
+            final MediaResourceGateway mediaResourceGateway) {
         this.videoGateway = Objects.requireNonNull(videoGateway);
+        this.mediaResourceGateway = Objects.requireNonNull(mediaResourceGateway);
     }
 
     @Override
     public void execute(final String anIn) {
-        this.videoGateway.deleteById(VideoID.from(anIn));
+        final var aVideoId = VideoID.from(anIn);
+        this.videoGateway.deleteById(aVideoId);
+        this.mediaResourceGateway.clearResources(aVideoId);
     }
 }
