4c9178cdc36ae19f250b00ea2c46b513a0b0ea2b
Celso Pires <celso.pires1999@gmail.com>
2025-06-30 23:46:57 +0000

    25.12-criando-setup-de-testes-de-amqp

---
+++ infrastructure/src/test/java/com/cap/admin/catalogo/AmqpTest.java
@@ -0,0 +1,20 @@
+package com.cap.admin.catalogo;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.cap.admin.catalogo.infrastructure.configuration.WebServerConfig;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-integration")
+@SpringBootTest(classes = WebServerConfig.class)
+public @interface AmqpTest {
+}
